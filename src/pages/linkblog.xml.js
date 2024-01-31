9d703d19c1bde053cbdf79be9787da901512e378
Luke Davis <luke@lukealexdavis.co.uk>
2024-01-31 16:53:54 +0000

    updated linkblog + styling

--- src/pages/linkblog.xml.js
+++ src/pages/linkblog.xml.js
@@ -4,10 +4,10 @@ import site from '../data/site.json';
 
 export async function get(context) {
   return rss({
-    title: site.title,
-    description: site.description,
-    site: context.site,
-    stylesheet: './style/pretty-feed-v3.xsl',
+    title: "Luke's Linkblog",
+    description: "A linkblog about all kinds of topics including SEO, AI, music, life, and the Web.",
+    site: site.url + 'linkblog',
+    stylesheet: './style/linkblog-pretty-feed-v3.xsl',
     items: linkblog.map((post) => ({
       title: post.title,
       description: post.description,
