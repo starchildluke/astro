81a93e764fee38789bb7650476c69d51ffbaf183
Luke Davis <luke@lukealexdavis.co.uk>
2025-05-01 21:28:46 +0100

    new build

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,5 @@
 
 export default new Map([
 ["src/content/morsels/morsel-16.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmorsels%2Fmorsel-16.mdx&astroContentModuleFlag=true")],
-["src/content/recortes/wander-in-porto.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecortes%2Fwander-in-porto.mdx&astroContentModuleFlag=true")],
-["src/content/posts/some-genuine-thoughts-vibe-coding.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fsome-genuine-thoughts-vibe-coding.mdx&astroContentModuleFlag=true")]]);
+["src/content/recortes/wander-in-porto.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecortes%2Fwander-in-porto.mdx&astroContentModuleFlag=true")]]);
 		
