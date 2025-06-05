398c8f80b626a358882c0103541e83e8530feca1
Luke Davis <luke@lukealexdavis.co.uk>
2025-06-05 16:42:06 +0100

    new post

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -3,5 +3,7 @@ export default new Map([
 ["src/content/morsels/morsel-16.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmorsels%2Fmorsel-16.mdx&astroContentModuleFlag=true")],
 ["src/content/recortes/wander-in-porto.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecortes%2Fwander-in-porto.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/some-genuine-thoughts-vibe-coding.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fsome-genuine-thoughts-vibe-coding.mdx&astroContentModuleFlag=true")],
-["src/content/posts/short-form-blogs-subversive.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fshort-form-blogs-subversive.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/short-form-blogs-subversive.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fshort-form-blogs-subversive.mdx&astroContentModuleFlag=true")],
+["src/content/posts/good-faith-ai-you-first.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fgood-faith-ai-you-first.mdx&astroContentModuleFlag=true")],
+["src/content/posts/some-brief-thoughts-on web-dev-and-web-performance.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fsome-brief-thoughts-on+web-dev-and-web-performance.mdx&astroContentModuleFlag=true")]]);
 		
