cbec633243182bcb13a21a10ba5a24315dcea389
Luke Davis <luke@lukealexdavis.co.uk>
2025-06-11 01:16:37 +0100

    new morsel

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -5,5 +5,6 @@ export default new Map([
 ["src/content/posts/some-genuine-thoughts-vibe-coding.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fsome-genuine-thoughts-vibe-coding.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/short-form-blogs-subversive.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fshort-form-blogs-subversive.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/good-faith-ai-you-first.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fgood-faith-ai-you-first.mdx&astroContentModuleFlag=true")],
-["src/content/posts/some-brief-thoughts-on web-dev-and-web-performance.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fsome-brief-thoughts-on+web-dev-and-web-performance.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/some-brief-thoughts-on web-dev-and-web-performance.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fsome-brief-thoughts-on+web-dev-and-web-performance.mdx&astroContentModuleFlag=true")],
+["src/content/morsels/morsel-20.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmorsels%2Fmorsel-20.mdx&astroContentModuleFlag=true")]]);
 		
