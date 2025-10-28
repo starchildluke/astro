89b5e49c18f127f8eb61f5a4b73bdba01279b440
Luke Davis <luke@lukealexdavis.co.uk>
2025-10-28 19:28:34 +0000

    updated astro

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,10 +1,10 @@
 
 export default new Map([
 ["src/content/morsels/morsel-16.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmorsels%2Fmorsel-16.mdx&astroContentModuleFlag=true")],
-["src/content/morsels/morsel-20.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmorsels%2Fmorsel-20.mdx&astroContentModuleFlag=true")],
 ["src/content/morsels/morsel-23.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmorsels%2Fmorsel-23.mdx&astroContentModuleFlag=true")],
-["src/content/posts/apis-vs-chatbots.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fapis-vs-chatbots.mdx&astroContentModuleFlag=true")],
+["src/content/morsels/morsel-20.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmorsels%2Fmorsel-20.mdx&astroContentModuleFlag=true")],
 ["src/content/recortes/wander-in-porto.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Frecortes%2Fwander-in-porto.mdx&astroContentModuleFlag=true")],
+["src/content/posts/apis-vs-chatbots.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fapis-vs-chatbots.mdx&astroContentModuleFlag=true")],
 ["src/content/morsels/morsel-28.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmorsels%2Fmorsel-28.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/good-faith-ai-you-first.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fgood-faith-ai-you-first.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/github-as-a-cms.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fgithub-as-a-cms.mdx&astroContentModuleFlag=true")],
